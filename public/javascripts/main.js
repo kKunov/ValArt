803393e9f847f1ba3179a8d2dcc1335895b6b22b
Kaloian Kunov <kkunov121@gmail.com>
2018-12-15 18:47:37 +0200

    new design of the galleri is ready

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,10 @@
 !function () {
-    function galleriesClicked() {
-        $(".k-page-views").attr("style" ,"display:none;");
-        $(".k-galleries").removeAttr("style");
-    }
-
     function homeBtnClicked() {
         $(".k-page-views").attr("style" ,"display:none;");
         $(".homeInfo").removeAttr("style");
     }
 
     function galClicked(e) {
-        var galleryName = $(this).attr("data-gal-id");
         var $indexatorContainer = $(".k-indexator-container"),
             $imagesContainer = $(".k-images-container"),
             classActive = "\" class=\"active\"",
@@ -21,7 +15,7 @@
         $indexatorContainer.empty();
         $imagesContainer.empty();
 
-        jQuery.get('galleries/galleriNamesInFolder?galleryName=' + galleryName, function(result) {
+        jQuery.get('galleries/galleriNamesInFolder', function(result) {
             for (i = 0; i < result.length; i++){
                 $indexatorContainer.append(
                     "<li data-target=\"#k-gallery-slider\" data-slide-to=\"" + i + (i === 0 ? classActive : "") + "\"></li>"
@@ -29,13 +23,36 @@
 
                 $imagesContainer.append(
                     "<div class=\"item" + (i === 0 ? " active" : "") + "\">" + 
-                    "<img src=\"images/gal/" + galleryName + "/" + result[i] + "\"></div>"
+                    "<img src=\"images/gal/" + result[i] + "\"></div>"
                 );
             }
+
+            nextPrevBtnClicked();
         });
     }
     
-    $("body").on("click", ".k-galleries-navbar", galleriesClicked);
+    function nextPrevBtnClicked(e) {
+        var $slider = $('#k-gallery-slider'),
+            $img = $slider.find(".active > img"),
+            imgHeight = $img.height(),
+            imgWidth = $img.width(),
+            newImgWidth;
+
+        if (imgHeight > imgWidth) {
+            $img.attr("style", "max-width: none; max-height: 61vh;");
+            newImgWidth = $img.width();
+            $slider.attr("style", "max-width: " + newImgWidth + "px;");
+            $slider.closest(".k-single-gal").attr("style", "left: 56vw;");
+        }
+        else {
+            $slider.removeAttr("style");
+            $img.removeAttr("style");
+            $slider.closest(".k-single-gal").removeAttr("style");
+        }
+    }
+
+    $("body").on("click", ".k-galleries-navbar", galClicked);
     $("body").on("click", ".k-home-navbar", homeBtnClicked);
-    $("body").on("click", ".k-gallery-single-pic", galClicked);
+    $("body").on("slid.bs.carousel", "#k-gallery-slider", nextPrevBtnClicked);
+    $(window).on('resize', nextPrevBtnClicked);
 }();
--- routes/galleries.js
+++ routes/galleries.js
@@ -4,7 +4,7 @@ var path = require('path');
 var fs = require('fs');
 
 router.get('/galleriNamesInFolder', function(req, res) {
-    fs.readdir(path.join(__dirname, "../public/images/gal", req.query.galleryName), function(err, files) {
+    fs.readdir(path.join(__dirname, "../public/images/gal"), function(err, files) {
         if (err) {
             console.log(err);
             res.sendStatus(500);
